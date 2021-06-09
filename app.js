e3ebeb636b45ecfd81bafe6eda75d64b87ec47d9
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-06-09 13:03:06 +0500

    commit1

---
+++ app.js
@@ -0,0 +1,32 @@
+// <!-- ***************************** Task : 05 **************************** -->
+// This will work with task 05
+
+// var clicks = 0;
+// function increase() {
+//     console.log(  clicks += 1);
+//     }
+
+    
+// function decrease() {
+//     if(clicks > 0){
+
+//         console.log(  clicks -= 1);
+//     }else{
+//         clicks = false
+//     }
+//     }
+
+//     document.write(" <br> open console to see counter")
+
+
+
+
+// <!-- ***************************** Task : 03 **************************** -->
+// This will work with task 03
+
+
+// function myfunction(tr){
+//     var i = tr.parentNode.parentNode.rowIndex;
+//     document.getElementById("myTable").deleteRow(i);
+
+// }
